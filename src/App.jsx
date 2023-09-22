6e43fcd11ec6e233a16f43737bb2b6690d9d257a
rc-balaji <balajic@drngpit.ac.in>
2023-09-22 23:12:14 +0530

    aaa

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import UserSearch from './UserSearch';
+
+function App() {
+  return (
+    <div className="App">
+      <UserSearch />
+    </div>
+  );
+}
+
+export default App;
